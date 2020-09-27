b0f03ae480a9ea95deb393a21ab161205a206405
Emerson Almeida <emersonalmeida@live.com>
2020-09-27 03:07:38 -0300

    Ajustes...

--- js/scoll-reveal.js
+++ js/scoll-reveal.js
@@ -17,7 +17,7 @@ const target = document.querySelectorAll('[data-motion]');
 const animationClass = 'animate'
 
 function animeScroll() {
-    const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 3.5);
+    const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4);
     target.forEach(function(element){
         if((windowTop) > element.offsetTop) {
             element.classList.add(animationClass);
